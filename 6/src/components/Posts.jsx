0ece75e1bbde7808ce63ba57b81e9027d03f2b51
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-11-23 20:33:24 +0300

    Выполнено упражнение 6

--- 6/src/components/Posts.jsx
+++ 6/src/components/Posts.jsx
@@ -5,7 +5,12 @@ import Post from './Post.jsx';
 
 const Posts = () => {
   // BEGIN (write your solution here)
+  const posts = useSelector((state) => {
+    const { ids, entities } = state.postsReducer;
+    return ids.map((id) => entities[id]);
+  });
 
+  return posts.map((post) => <Post key={post.id} post={post} />);
   // END
 };
 
