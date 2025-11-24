0a19b7ae52df063bc4f8274b10518cfb62e315f4
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-11-24 10:15:49 +0300

    Выполнено упражнение 7

--- 7/src/slices/usersSlice.js
+++ 7/src/slices/usersSlice.js
@@ -1,5 +1,21 @@
 import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
 
 // BEGIN (write your solution here)
+const usersAdapter = createEntityAdapter();
 
+const initialState = usersAdapter.getInitialState();
+
+const usersSlice = createSlice({
+    name: "users",
+    initialState,
+    reducers: {
+        addUsers: (state, action) => {
+            usersAdapter.addMany(state, action.payload);
+        },
+    },
+});
+
+export const { actions } = usersSlice;
+export const selectors = usersAdapter.getSelectors((state) => state.users);
+export default usersSlice.reducer;
 // END
