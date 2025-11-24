01be6e2cf730d4081b4c987a25dcbcf17905dba0
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-11-24 12:08:06 +0300

    Выполнено упражнение 9

--- 9/src/components/Tasks.jsx
+++ 9/src/components/Tasks.jsx
@@ -2,13 +2,19 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 // BEGIN (write your solution here)
-
+import { fetchTasks, removeTask, selectTasks } from '../slices/tasksSlice.js';
 // END
 
 const Tasks = () => {
   const dispatch = useDispatch();
   // BEGIN (write your solution here)
-
+  const tasks = useSelector(selectTasks);
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+  const handleRemoveTask = (id) => {
+    dispatch(removeTask(id));
+  };
   // END
 
   return tasks && (
--- 9/src/slices/tasksSlice.js
+++ 9/src/slices/tasksSlice.js
@@ -13,5 +13,68 @@ export const fetchTasks = createAsyncThunk("tasks/fetchTasks", async () => {
 });
 
 // BEGIN (write your solution here)
+export const addTask = createAsyncThunk("tasks/addTask", async (name) => {
+  const response = await axios.post(routes.tasksPath(), { name });
+  return response.data;
+});
 
+export const removeTask = createAsyncThunk("tasks/removeTask", async (id) => {
+  await axios.delete(routes.taskPath(id));
+  return id;
+});
+
+const tasksAdapter = createEntityAdapter();
+
+const tasksSlice = createSlice({
+  name: "tasks",
+  initialState: tasksAdapter.getInitialState({loadingState: "idle", error: null}),
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTasks.pending, (state) => {
+        state.loadingState = "loading";
+        state.error = null;
+      })
+      .addCase(fetchTasks.fulfilled, (state, action) => {
+        tasksAdapter.setAll(state, action.payload);
+        state.loadingState = "idle";
+        state.error = null;
+      })
+      .addCase(fetchTasks.rejected, (state, action) => {
+        state.loadingState = "failed";
+        state.error = action.error.message;
+      })
+      .addCase(addTask.pending, (state) => {
+        state.loadingState = "loading";
+        state.error = null;
+      })
+      .addCase(addTask.fulfilled, (state, action) => {
+        tasksAdapter.addOne(state, action.payload);
+        state.loadingState = "idle";
+        state.error = null;
+      })
+      .addCase(addTask.rejected, (state, action) => {
+        state.loadingState = "failed";
+        state.error = action.error.message;
+      })
+      .addCase(removeTask.pending, (state) => {
+        state.loadingState = "loading";
+        state.error = null;
+      })
+      .addCase(removeTask.fulfilled, (state, action) => {
+        tasksAdapter.removeOne(state, action.payload);
+        state.loadingState = "idle";
+        state.error = null;
+      })
+      .addCase(removeTask.rejected, (state, action) => {
+        state.loadingState = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const { selectAll: selectTasks } = tasksAdapter.getSelectors(
+  (state) => state.tasks
+);
+export default tasksSlice.reducer;
 // END
